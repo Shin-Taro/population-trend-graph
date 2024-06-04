6af128901cfb2afc39f96fa38ff9e85878df1430
Shin-Taro <sh.takahashi.113@gmail.com>
2024-06-05 00:22:59 +0900

    feat: implement pages component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
-import { NextPage } from "next"
-import styled from "styled-components"
+import { GraphController } from "@/components/GraphController"
+import { GetServerSideProps, NextPage } from "next"
+import { ComponentPropsWithoutRef } from "react"
+import { resasApiClient } from "@/shared/utiles/resasApiClient"
 
-const Home: NextPage = () => <TestDiv>temporary</TestDiv>
+type PageProps = ComponentPropsWithoutRef<typeof GraphController>
 
-const TestDiv = styled.div`
-  color: red;
-`
+const Home: NextPage<PageProps> = (props) => <GraphController {...props} />
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
+  // TODO: エラーハンドリング
+
+  const response = await resasApiClient.getPrefectures()
+  return { props: { prefecturesData: response } }
+}
 
 export default Home
