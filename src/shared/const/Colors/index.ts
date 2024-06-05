0fcd67a2cd7407f02b0e822acc2fe861c0b83fb3
Shin-Taro <sh.takahashi.113@gmail.com>
2024-06-06 01:09:13 +0900

    feat: add color code constant

--- src/shared/const/Colors/index.ts
+++ src/shared/const/Colors/index.ts
@@ -2,7 +2,11 @@ type ColorKey = `COLOR_${string}`
 type ColorValue = `#${string}`
 
 const COLOR_C4C4C4 = "#c4c4c4"
+const COLOR_636363 = "#636363"
+const COLOR_075FB1 = "#075fb1"
 
 export const Colors = {
   COLOR_C4C4C4,
+  COLOR_636363,
+  COLOR_075FB1,
 } as const satisfies Record<ColorKey, ColorValue>
