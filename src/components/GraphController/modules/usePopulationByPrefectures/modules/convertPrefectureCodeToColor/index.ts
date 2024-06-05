b134b957cdddeb38455f4b4f14d474582ffa48de
Shin-Taro <sh.takahashi.113@gmail.com>
2024-06-06 02:49:07 +0900

    feat: improvement to avoid white color

--- src/components/GraphController/modules/usePopulationByPrefectures/modules/convertPrefectureCodeToColor/index.ts
+++ src/components/GraphController/modules/usePopulationByPrefectures/modules/convertPrefectureCodeToColor/index.ts
@@ -1,6 +1,6 @@
 // TODO: 適当過ぎるので改善する
 export const convertPrefectureCodeToColor = (prefectureCode: number): string => {
-  const largeNumber = prefectureCode * 999
-  const colorCode = largeNumber.toString(16).slice(0, 6)
-  return `#${colorCode}`
+  const largeNumber = prefectureCode * 999_999_999
+  const hex = largeNumber.toString(16)
+  return `#${hex[0]}0${hex[2]}f${hex[4]}b`
 }
